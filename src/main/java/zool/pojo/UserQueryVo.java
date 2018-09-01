a21bec98cfd218d36eb95ef1787fe6d299e7d255
zoolye <www.iphone@foxmail.com>
2018-09-01 15:31:30 +0800

    feat:添加sql片段

--- src/main/java/zool/pojo/UserQueryVo.java
+++ src/main/java/zool/pojo/UserQueryVo.java
@@ -2,6 +2,8 @@ package zool.pojo;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author : zoolye
  * @date : 2018-07-22 16:04
@@ -14,5 +16,6 @@ public class UserQueryVo {
     //用户查询条件
     private UserCustom userCustom;
 
+    private List<Integer> ids;
 
 }
